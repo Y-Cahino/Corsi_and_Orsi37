c0df9ca91cec76575dde2da13dd600a3409c89db
cahinosilva.20184 <cahinosilva.20184@LABI2-13PC05>
2023-01-13 12:16:36 +0100

    Aggiungere i file di progetto.

---
+++ Corsi_and_Orsi37/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Corsi_and_Orsi37
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //declare
+            Random r = new Random();
+            int[] array = new int[10];
+            //generazione array input
+
+            //aggiunta valore
+            for(int i = 0; i < array.Length; i++)
+            {
+                array[i] = r.Next(1, 11);
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
