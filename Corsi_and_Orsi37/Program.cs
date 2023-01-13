fc90b295c3dd7a745b27bb483b73ed8ae7ca6ae4
cahinosilva.20184 <cahinosilva.20184@LABI2-13PC05>
2023-01-13 12:42:49 +0100

    thread sleep

--- Corsi_and_Orsi37/Program.cs
+++ Corsi_and_Orsi37/Program.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace Corsi_and_Orsi37
 {
@@ -10,20 +10,30 @@ namespace Corsi_and_Orsi37
     {
         static void Main(string[] args)
         {
+            //declare
+            int[] array = new int[100];
+            genaarray(array);
         }
-        void genaarray()
+        static void genaarray(int[] array)
         {
             //declare
             Random r = new Random();
             //generazione array input
             Console.WriteLine("Inserire numero elementi array");
             int x = Convert.ToInt32(Console.ReadLine());
-            int[] array = new int[x];
+            array = new int[x];
             //generazaione casuale array
             for (int i = 0; i < array.Length; i++)
             {
                 array[i] = r.Next(1, 100);
                 Console.WriteLine(array[i]);
+            }
+            //aggiunta valore
+            Console.WriteLine("Aggiunta valore in corso...");
+            Thread.Sleep(1000);
+            for()
+            {
+
             }
         }
         
