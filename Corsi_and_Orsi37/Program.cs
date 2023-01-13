4a311392cc62e2fb1689d356de58ce299f9b7e55
cahinosilva.20184 <cahinosilva.20184@LABI2-13PC05>
2023-01-13 12:26:02 +0100

    void

--- Corsi_and_Orsi37/Program.cs
+++ Corsi_and_Orsi37/Program.cs
@@ -10,19 +10,22 @@ namespace Corsi_and_Orsi37
     {
         static void Main(string[] args)
         {
+        }
+        void genaarray()
+        {
             //declare
             Random r = new Random();
-            
             //generazione array input
             Console.WriteLine("Inserire numero elementi array");
-           int x = Convert.ToInt32(Console.ReadLine());
+            int x = Convert.ToInt32(Console.ReadLine());
             int[] array = new int[x];
             //generazaione casuale array
             for (int i = 0; i < array.Length; i++)
             {
-                array[i] = r.Next(1, 11);
+                array[i] = r.Next(1, 100);
                 Console.WriteLine(array[i]);
             }
         }
+        
     }
 }
